303629f566705f41a7402cf7bc3cc901a7a78696
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-09-10 17:43:14 +0900

    git init

---
+++ recipe.rb
@@ -0,0 +1,26 @@
+%w[
+hsbt
+mame
+k0kubun
+nobu
+].each do |u|
+  user u do
+    action :create
+  end
+
+  directory "/home/#{u}" do
+    mode  "755"
+    owner u
+  end
+
+  directory "/home/#{u}/.ssh" do
+    mode  "700"
+    owner u
+  end
+
+  file "/home/#{u}/.ssh/authorized_keys" do
+    mode  "600"
+    owner u
+    source "keys/#{u}.keys"
+  end
+end
