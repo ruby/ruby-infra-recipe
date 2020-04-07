27a9067c554514e8abfee4ed629d3536b0bee553
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-04-07 11:58:30 +0900

    Fixed the error with dryrun

--- recipes/gem-codesearch.rb
+++ recipes/gem-codesearch.rb
@@ -1,9 +1,9 @@
-merged_key = Dir.glob(File.expand_path("keys", __dir__) + "/*").map do|key|
+merged_key = Dir.glob(File.expand_path("../keys", __FILE__) + "/*").map do|key|
   File.read(key)
 end.join
 
-remote_file "/home/ubuntu/.ssh/authorized_keys" do
-  source merged_key
+file "/home/ubuntu/.ssh/authorized_keys" do
+  content merged_key
   mode  '600'
   owner 'ubuntu'
 end
