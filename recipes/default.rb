590660043f7ccfde59e798295219983dd20fbe63
Yusuke Endoh <mame@ruby-lang.org>
2020-05-01 15:18:40 +0900

    Factor out account creation to recipes/setup-users.rb
    and create all accounts on gem-codesearch server

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,44 +1,4 @@
-%w[
-  eregon
-  hsbt
-  k0kubun
-  knu
-  ko1
-  mame
-  nobu
-  shyouhei
-  znz
-].each do |u|
-  user u do
-    case node[:platform]
-    when 'debian', 'ubuntu'
-      gid 27 # sudo
-      shell '/bin/bash'
-    when 'freebsd', 'openbsd'
-      gid 0 # wheel
-    when 'opensuse'
-      gid 100 # users (workaround no wheel)
-    else
-      gid 10 # wheel
-    end
-  end
-
-  directory "/home/#{u}" do
-    mode  '755'
-    owner u
-  end
-
-  directory "/home/#{u}/.ssh" do
-    mode  '700'
-    owner u
-  end
-
-  remote_file "/home/#{u}/.ssh/authorized_keys" do
-    source "keys/#{u}.keys"
-    mode  '600'
-    owner u
-  end
-end
+include_recipe "setup-users"
 
 user 'chkbuild' do
   case node[:platform]
---
+++ recipes/setup-users.rb
@@ -0,0 +1,32 @@
+Dir.glob(File.expand_path("../keys", __FILE__) + "/*.keys").sort.each do |key|
+  u = key[/\/([^\/]+)\.keys$/, 1] # u = File.basename(key, ".keys")
+  user u do
+    case node[:platform]
+    when 'debian', 'ubuntu'
+      gid 27 # sudo
+      shell '/bin/bash'
+    when 'freebsd', 'openbsd'
+      gid 0 # wheel
+    when 'opensuse'
+      gid 100 # users (workaround no wheel)
+    else
+      gid 10 # wheel
+    end
+  end
+
+  directory "/home/#{u}" do
+    mode  '755'
+    owner u
+  end
+
+  directory "/home/#{u}/.ssh" do
+    mode  '700'
+    owner u
+  end
+
+  remote_file "/home/#{u}/.ssh/authorized_keys" do
+    source "keys/#{u}.keys"
+    mode  '600'
+    owner u
+  end
+end
